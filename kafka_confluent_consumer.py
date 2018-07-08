b6261e3e2ed0f8212fefcacadb15677fb2549de2
Amir <pangan@gmail.com>
2018-07-08 20:01:07 +0200

    Create kafka_confluent_consumer.py

---
+++ kafka_confluent_consumer.py
@@ -0,0 +1,57 @@
+from confluent_kafka import Consumer, KafkaError
+
+def amir_commit(b, c):
+
+    print ('Commit!!! {}'.format(b))
+
+def read_kafka():
+
+    c = Consumer({
+        'bootstrap.servers': 'amir-dev.dev.op5.com',
+        'group.id': 'mygroup3',
+        'client.id': 'client-2',
+        'enable.auto.commit': True,
+        'session.timeout.ms': 6000,
+        'default.topic.config': {'auto.offset.reset': 'beginning'}
+    })
+
+
+    c.subscribe(['amir-topic-1'])
+
+    try:
+        while True:
+
+
+            msg = c.poll(0)
+
+            if msg is None:
+               # yield None
+                continue
+
+            if msg.error():
+                print (msg.error())
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+
+                    #yield None
+                    continue
+                else:
+                    #yield str(msg.error())
+                    print(msg.error())
+                    break
+            print ('Received message: {}'.format(msg.value().decode('utf-8')))
+            #yield 'Received message: {}'.format(msg.value().decode('utf-8'))
+            #yield '->{}'.format(msg)
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        print("closing ...")
+        c.close()
+
+
+kf=read_kafka()
+# while True:
+#
+#     a = next(kf)
+#     if a:
+#         print(a)
